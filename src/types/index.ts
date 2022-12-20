ebd6414f51561906acda74b89cbcdfc49d35871b
mrevanzak <mrevanzak@gmail.com>
2022-12-20 22:36:15 +0700

    refactor: delete unused code

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,29 +1,3 @@
-export type Products = {
-  id: number;
-  title: string;
-  price: string;
-  category: string;
-  description: string;
-  image: string;
-  quantity: number;
-  rating: {
-    rate: number;
-    count: number;
-  };
-};
-
-export type Cart = Products;
-
-export type User = {
-  username: string;
-  password: string;
-};
-
-export type UserResponse = {
-  token: string | null;
-  admin?: boolean;
-};
-
 export type MovieResponse = {
   Response: string;
   totalResults: string;
